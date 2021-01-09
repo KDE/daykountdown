fcc78f85e8ca8807233cdaa2f8f6dafc1292a165
Claudio Cambra <claudio.cambra@gmail.com>
2021-01-09 16:07:26 +0100

    Added comments explaning code

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 #include <KMessageBox>
 
 int main (int argc, char *argv[]) {
+    // Must create QApplication object first, and only once
     QApplication app(argc, argv);
-    KLocalizedString::setApplicationDomain("DayCountdown");
+    KLocalizedString::setApplicationDomain("DayCountdown"); //Properly sets translation catalog
     
+    // Class used to store information about the program
+    // Every KDE application should have this
     KAboutData aboutData(
         // Internal program name
         QStringLiteral("DayCountdown"),
@@ -31,16 +34,32 @@ int main (int argc, char *argv[]) {
     
     aboutData.addAuthor(i18n("Claudio Cambra"), i18n("Task"), QStringLiteral("claudio.cambra@gmail.com"), QStringLiteral("https://github.com/elChupaCambra/"));
     
+    // Initialise the properties of the QApplication object
     KAboutData::setApplicationData(aboutData);
     
+    // Class used to specify command line switches, i.e. to open up app with specific file
+    // Here it just opens the app
     QCommandLineParser parser;
     aboutData.sewtupCommandLine(&parser);
     parser.process(app);
     aboutData.processCommandLine(&parser);
     
+    // Pop-up box with custom buttons
+    /* KGuiItem is an object constructor: 
+     * the first arg is the text on the button, 
+     * the second is whether to add an icon (empty),
+     * the third is the tooltip given when hovering over the button,
+     * the fourth is a help text provided by right-clicking */
     KGuiItem yesButton(i18n("Hello"), QString(),
                        i18n("This is a tooltip"),
                        i18n("This is a WhatsThis help text."));
     
-    return KMessageBox::questionYesNo(0, i18n("Hello World"), i18n("Hello"), yesButton) == KMessageBox::Yes? EXIT_SUCCESS: EXIT:FAILURE;
+    // Create message box with a yes and no button
+    /* Arguments:
+     * second argument is the tect that will appear above the buttons
+     * third is the caption the window will have
+     * finally we set the KGuiItem for the "Yes" button to the KGuiItem yesButton we created*/
+    return KMessageBox::questionYesNo(0, i18n("Hello World"), i18n("Hello"), yesButton) == KMessageBox::Yes? EXIT_SUCCESS : EXIT_FAILURE;
 }
+
+// All user-visible text is passed through the i18n() function, necessary for the UI to be translatable.
