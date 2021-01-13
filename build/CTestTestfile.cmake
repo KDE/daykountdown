c184f2fc73cc10953ed952b73b26b58c49f5a031
elChupaCambra <claudio.cambra@gmail.com>
2021-01-13 13:30:43 +0100

    Fixed CMakeLists, filled in QML file. Now works

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/claucambra/Downloads/GitHub/DayKountdown
-# Build directory: /home/claucambra/Downloads/GitHub/DayKountdown/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(appstreamtest "/usr/bin/cmake" "-DAPPSTREAMCLI=/usr/bin/appstreamcli" "-DINSTALL_FILES=/home/claucambra/Downloads/GitHub/DayKountdown/build/install_manifest.txt" "-P" "/usr/share/ECM/kde-modules/appstreamtest.cmake")
-set_tests_properties(appstreamtest PROPERTIES  _BACKTRACE_TRIPLES "/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;163;add_test;/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;181;appstreamtest;/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;0;;/home/claucambra/Downloads/GitHub/DayKountdown/CMakeLists.txt;17;include;/home/claucambra/Downloads/GitHub/DayKountdown/CMakeLists.txt;0;")
-subdirs("src")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/claucambra/Downloads/GitHub/DayKountdown/src
-# Build directory: /home/claucambra/Downloads/GitHub/DayKountdown/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
