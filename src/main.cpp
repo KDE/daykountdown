9c91642b1ba787ec07f60071ba76adee5202b08c
Carl Schwan <carl@carlschwan.eu>
2021-01-19 21:04:49 +0100

    Fix i18n and control style
    i18n function in QML need KI18n and QApplication is needed for native
    controls in a QML application in Plasma.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
-#include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QtQml>
+#include <KLocalizedContext>
+#include <KLocalizedString>
 #include <QUrl>
 #include "backend.h"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
+    KLocalizedString::setApplicationDomain("daykountdown");
+    QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("KDE");
     QCoreApplication::setOrganizationDomain("kde.org");
     QCoreApplication::setApplicationName("DayKountdown");
@@ -18,6 +21,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 	engine.rootContext()->setContextProperty("backend", &backend);
 
     engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
+    engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
 
     if (engine.rootObjects().isEmpty()) {
         return -1;
