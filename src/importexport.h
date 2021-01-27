d5c022b095f4b2a336e85308ea8a482a5c07c988
Claudio Cambra <claudio.cambra@gmail.com>
2021-01-27 23:49:28 +0100

    Writing JSON import code

--- src/importexport.h
+++ src/importexport.h
@@ -1,1 +1,35 @@
+#pragma once
 
+#include <QQmlApplicationEngine>
+#include <QtQml>
+#include <QUrl>
+
+typedef struct kountdown {
+	Q_GADGET
+	Q_PROPERTY(int index MEMBER index);
+	Q_PROPERTY(QString name MEMBER name);
+	Q_PROPERTY(QString description MEMBER description);
+	Q_PROPERTY(QString date MEMBER date);
+	
+	public:
+		int index;
+		QString name;
+		QString description;
+		QString date;
+} kountdown;
+
+class ImportExport : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QVariantList Kountdowns READ kountdownPopulator)
+
+public:
+    explicit ImportExport(QObject *parent = nullptr);
+	QVariantList Kountdowns;
+	QVariantList kountdownPopulator();
+	
+private:	
+	QVector<kountdown> _kountdownArray;
+	
+	void _fetchKountdowns();
+};
