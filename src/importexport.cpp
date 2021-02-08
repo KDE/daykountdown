c00e96386d5fca5a5eb2a4c27a5a07d2e080c83c
Claudio Cambra <claudio.cambra@gmail.com>
2021-02-08 20:45:58 +0100

    Added comments to importexport.cpp

--- src/importexport.cpp
+++ src/importexport.cpp
@@ -7,17 +7,23 @@
 #include "importexport.h"
 #include "kountdownmodel.h"
 
+// Constructor function
 ImportExport::ImportExport(QObject *parent) : QObject(parent)
 {
 
 }
 
+// Function extracts koutndowns from SQLite table and converts to JSON array
 QJsonDocument ImportExport::_createJson() {
 	QJsonArray kountdownsJsonArr;
 	
+	// Select all rows from KountdownModel table
 	QSqlQuery query("SELECT * FROM KountdownModel");
+	// Query.next moves through each returned row
 	while(query.next()) {
+		// Object to add to JSON array
 		QJsonObject kountdownToAdd {
+			// Query.value(number) <- number is the column
 			{"name", query.value(1).toString()},
 			{"description", query.value(2).toString()},
 			{"date", query.value(3).toString()},
@@ -25,38 +31,52 @@ QJsonDocument ImportExport::_createJson() {
 		};
 		kountdownsJsonArr.append(kountdownToAdd);
 	}
+	// Create object with key-value pair of "kountdowns": kountdownsJsonArr
 	QJsonObject mainObj {{"kountdowns", kountdownsJsonArr}};
+	// Create JSON document from this object
 	QJsonDocument exportingDoc(mainObj);
 	return exportingDoc;
 }
 
 void ImportExport::exportFile() {
+	// Open file dialog to get path for file-name to save
 	QString fileName = QFileDialog::getSaveFileName(NULL, i18n("Save File As"), "exported_kountdowns.json", "JSON (*.json)");
+	// Get the JSON file from the previous function to create
 	QJsonDocument jsonDoc = _createJson();
 	QSaveFile file(fileName);
 	file.open(QIODevice::WriteOnly);
 	file.write(jsonDoc.toJson());
+	// Commit writes the changes to file
 	file.commit();
 }
 
 void ImportExport::fetchKountdowns() {
+	// _kountdownArray is going to hold all out kountdowns extracted from the JSON
+	// We clear it so we don't import kountdowns from previously imported files
 	_kountdownArray.clear();
 	
+	// Get QUrl for file to import
 	QUrl filePath = QFileDialog::getOpenFileUrl(NULL, i18n("Import file"));
+	// Returns the path of the QUrl formatted as a local gile path
 	filePath = filePath.toLocalFile();
 	
 	QFile inFile(filePath.toString());
 	if(inFile.exists()) {
 		qDebug() << "Found kountdowns.json";
 		inFile.open(QIODevice::ReadOnly | QIODevice::Text);
+		// QByteArray is going to hold the data from the JSON file
 		QByteArray data = inFile.readAll();
 		inFile.close();
 		
+		// errorPtr is going to contain any errors we run into when parsing our JSON data
 		QJsonParseError errorPtr;
+		// Create internal JSON doc from data, with errorPtr address for errors
 		QJsonDocument kountdownsDoc = QJsonDocument::fromJson(data, &errorPtr);
 		if(kountdownsDoc.isNull())
 			qDebug() << "Parse failed";
+		// Create JSON object from root object of JSON file
 		QJsonObject rootObj = kountdownsDoc.object();
+		// Create array from the root object's value of "kountdowns" key (which is an array)
 		QJsonArray kountdownsJsonArray = rootObj.value("kountdowns").toArray();
 		
 		/*
@@ -75,8 +95,11 @@ void ImportExport::fetchKountdowns() {
 		*/
 		
 		int i = 0;
+		// For each kountdown in the JSON array...
 		foreach(const QJsonValue & kountdownJson, kountdownsJsonArray) {
+			// Create kountdown struct
 			kountdown currKountdown;
+			// Set values of this struct
 			currKountdown.index = i;
 			currKountdown.name = kountdownJson.toObject().value("name").toString();
 			currKountdown.description = kountdownJson.toObject().value("description").toString();
@@ -84,6 +107,7 @@ void ImportExport::fetchKountdowns() {
 			if(kountdownJson.toObject().contains("colour"))
 				currKountdown.colour = kountdownJson.toObject().value("colour").toString();
 			else
+				// Default text colour defined in QML
 				currKountdown.colour = "palette.text";
 			_kountdownArray.append(currKountdown);
 			i++;
